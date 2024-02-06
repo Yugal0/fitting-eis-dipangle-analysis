f6acecaaf7124c4cdfe7a4fb444dbe521c4336c9
Yugal0 <yugal2531@gmail.com>
2024-02-06 12:20:23 +0530

    latest

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from mpmath import *
-mp.dps=50
+mp.dps=300
 
 @element(num_params=3, units=["Ohm", "F", ""],overwrite=True)
 def TLMuni(p,f):
